7c2775b597f78d5684bcacbc8d5fb8f5a2d6add4
abdulnaeem76 <157393718+abdulnaeem76@users.noreply.github.com>
2024-01-24 20:44:57 -0500

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,17 +1,14 @@
 def hello_world():
-    # Write a doc string below. Doc strings can be used to provide the user with a
-    # description of what the function does.
     """
-
+    This function prints "Hello World!" to the console.
+    It takes no parameters and returns nothing.
     """
-
-    # Write a comment; comments are very useful to other programmers.
-
-    # Write a Python statement to print Hello, World!
+    print("Hello, World!")
+hello_world()
 
 def add(a,b):
 
     # Write simple code to add variable a and b
-    output =
+    output = a + b
 
     return output
